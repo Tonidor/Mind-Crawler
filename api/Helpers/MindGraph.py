3977f4b5a93bdb5f0bb0091b9919d809286c30e0
Tonidor <antonia.holsten@hotmail.de>
2019-03-31 11:37:32 +0200

    Graph data continued

--- api/Helpers/MindGraph.py
+++ api/Helpers/MindGraph.py
@@ -1,8 +1,8 @@
 from colour import Color
 from numpy import linalg as LAG
-import gensim
 import gensim.downloader as api
-
+from sklearn import cluster
+from sklearn import metrics
 
 colors = [
     Color("red"),
@@ -29,8 +29,11 @@ class Node:
     def __repr__(self):
         return str({"word": self.word, "color": self.color, "radius": self.radius, "group": self.group})
 
-    def add_occurences(self, times = 1):
+    def add_occurences(self, times=1):
         self.occurrences += times
+
+    def update_centroid(self, centroid):
+        self.color = centroid.calculate_color_interpolation(self.position)
 
 
 class Edge:
@@ -48,13 +51,24 @@ class MindGraph:
         self.words_used = set()
         self.nodes = {}
         self.edges = set()
+        self.topics_created = set()
         self.word2vec = api.load("glove-wiki-gigaword-100")
+        self.centroids = {}
+
+    def get_or_create_centroid(self, index, position):
+        if index in self.topics_created:
+            return self.centroids.get(index)
+        else:
+            t = TopicCentroid(index, position)
+            self.centroids[index] = t
+            self.topics_created.add(index)
+            return t
 
     def get_or_create_node(self, word, topic_centroid, position):
         if word in self.words_used:
             return self.nodes.get(word)
         else:
-            n = Node(topic_centroid,position, word)
+            n = Node(topic_centroid, position, word)
             self.nodes[word] = n
             self.words_used.add(word)
             return n
@@ -70,16 +84,44 @@ class MindGraph:
         return {"nodes": [repr(node) for key, node in self.nodes.items()],
                 "edges": [repr(edge) for edge in self.edges]}
 
+    def calculate_centroids(self):
+
+        kmeans = cluster.KMeans(n_clusters=6)
+        kmeans.fit(self.word2vec.vocab)
+
+        labels = kmeans.labels_
+        centroids = kmeans.cluster_centers_
+        max_distances = {}
+        for label, centroid in zip(labels, centroids):
+            self.get_or_create_centroid(label, centroid)
+
+        for word in self.words_used:
+            w2vpos = self.word2vec[word]
+
+            cluster_type = kmeans.predict(w2vpos)
+
+            distance = LAG.norm(w2vpos, cluster_type)
+
+            if distance > max_distances[cluster_type]:
+                max_distances[cluster_type] = distance
+
+            topic = self.get_or_create_centroid(cluster_type)
+            self.get_or_create_node(word, topic, w2vpos)
+
+        for topic in self.centroids:
+            topic.max_distance = max_distances[topic.index]
+
 
 class TopicCentroid:
-    def __init__(self, topic_index, position, max_distance):
+    def __init__(self, topic_index, position):
         self.color = colors[topic_index]
+        self.index = topic_index
         self.position = position
-        self.max_distance = max_distance
+        self.max_distance = 0
 
     def calculate_color_interpolation(self, target):
         distance = LAG.norm(self.position, target)
-        norm_distance = (self.max_distance - distance) / self.max_distance
+        norm_distance = abs(self.max_distance - distance) / self.max_distance
         temp = self.color.get_hsl()
         temp.set_luminance(self.color.get_luminance() * norm_distance)
         return temp
