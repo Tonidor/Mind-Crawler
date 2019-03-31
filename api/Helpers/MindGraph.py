285b35d395fde1a7403b9b50bba21073415671af
Tonidor <antonia.holsten@hotmail.de>
2019-03-31 09:43:39 +0200

    added as_dict for displaying graph

--- api/Helpers/MindGraph.py
+++ api/Helpers/MindGraph.py
@@ -1,47 +1,7 @@
 from colour import Color
 from numpy import linalg as LAG
-
-
-class Node:
-    def __init__(self, topic_centroid, position, word, occurrence):
-        self.color = topic_centroid.calculate_color_interpolation(position)
-        self.word = word
-        self.position = position
-        self.occurrences = 0
-
-    @@property
-    def radius(self):
-        return (self.occurrences / 10) + 5
-
-
-    def __repr__(self):
-        return str({"word": self.word, "color": self.color, "radius": self.radius})
-
-    def add_occurences(self, times = 1):
-        self.occurrences += times
-
-class Edge:
-    def __init__(self, source, target, weight):
-        self.source = source
-        self.target = target
-        self.weight = weight
-
-    def __repr__(self):
-        return str({"source": repr(self.source), "target": repr(self.target), "weight": self.weight})
-
-
-class MindGraph:
-    def __init__(self):
-        self.words_used = set()
-        self.nodes = {}
-
-    def get_or_create_node(self, word, ):
-        if word in self.words_used:
-            self.nodes.get(word)
-        else:
-            n = Node(word)
-            self.nodes[word] = n
-            return n
+import gensim
+import gensim.downloader as api
 
 
 colors = [
@@ -52,6 +12,63 @@ colors = [
     Color("orange"),
     Color("purple"),
 ]
+
+
+class Node:
+    def __init__(self, topic_centroid, position, word):
+        self.color = topic_centroid.calculate_color_interpolation(position)
+        self.word = word
+        self.position = position
+        self.occurrences = 0
+        self.group = topic_centroid.topic_index
+
+    @property
+    def radius(self):
+        return (self.occurrences / 10) + 5
+
+    def __repr__(self):
+        return str({"word": self.word, "color": self.color, "radius": self.radius, "group": self.group})
+
+    def add_occurences(self, times = 1):
+        self.occurrences += times
+
+
+class Edge:
+    def __init__(self, source, target, thickness):
+        self.source = source
+        self.target = target
+        self.thickness = thickness
+
+    def __repr__(self):
+        return str({"source": self.source.word, "target": self.target.word, "weight": self.thickness})
+
+
+class MindGraph:
+    def __init__(self):
+        self.words_used = set()
+        self.nodes = {}
+        self.edges = set()
+        self.word2vec = api.load("glove-wiki-gigaword-100")
+
+    def get_or_create_node(self, word, topic_centroid, position):
+        if word in self.words_used:
+            return self.nodes.get(word)
+        else:
+            n = Node(topic_centroid,position, word)
+            self.nodes[word] = n
+            self.words_used.add(word)
+            return n
+
+    def add_edge(self, target, source):
+        self.edges.add((target, source, self.word2vec_distance(target.word, source.word)))
+
+    def word2vec_distance(self, target, source):
+        return 1 - self.word2vec.distance(target, source)
+
+    @property
+    def as_dict(self):
+        return {"nodes": [repr(node) for key, node in self.nodes.items()],
+                "edges": [repr(edge) for edge in self.edges]}
 
 
 class TopicCentroid:
