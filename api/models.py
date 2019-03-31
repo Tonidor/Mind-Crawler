723002da10e59fe00c3bbc40874a02e61bc9d99c
Tonidor <antonia.holsten@hotmail.de>
2019-03-31 07:50:47 +0200

    added title to Entry module

--- api/models.py
+++ api/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Table
+from sqlalchemy import Column, Integer, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
@@ -7,14 +7,17 @@ Base = declarative_base()
 class Entry(Base):
     __tablename__ = "entry"
     id = Column('id', Integer, primary_key=True)
-    date = Column('date', String(8))
+    date = Column('date', DateTime)
+    title = Column('title', String(40))
     text_path = Column('text_path', String(50))
 
+    @property
     def as_dict(self):
         with open(self.text_path) as file:
             return {
                 'id': self.id,
                 'date': self.date,
+                'title': self.title,
                 'text': file.read()
             }
 
